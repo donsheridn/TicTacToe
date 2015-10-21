39218e9095ed757c22db735c6c2f501c746675a3
DonSheridn <donalsheridan@hotmail.com>
2015-10-20 17:22:47 -0700

    3 Refactoring complete.
    3 Refactoring complete.

--- src/TicTacToe.php
+++ src/TicTacToe.php
@@ -8,13 +8,9 @@
 
 namespace SDPHP\TTTGame;
 
-use Symfony\Component\HttpFoundation\Session\Session;
-use Symfony\Component\HttpFoundation\Request;
-
-
 class TicTacToe
 {
-    public $player = "X";            //whose turn it is
+    public $player = "X";           //whose turn it is
     public $totalMoves = 0;          //how many moves have been made so far
     public $boardvert;               //the tic tac toe board
 
@@ -30,7 +26,6 @@ class TicTacToe
             }
             $this->boardvert[] = $boardhoriz;
         }
-
         // print_r($this->boardvert);exit();
     }
 
@@ -59,7 +54,7 @@ class TicTacToe
 
                     if ($this->boardvert[$x][$y]) {         //check to see if that position already has an image
 
-                        echo "<img src=\"images/{$this->boardvert[$x][$y]}.jpg\" alt=\"{$this->boardvert[$x][$y]}\" title=\"{$this->boardvert[$x][$y]}\" />";
+                        echo "<img src=\"assets/images/{$this->boardvert[$x][$y]}.jpg\" alt=\"{$this->boardvert[$x][$y]}\" title=\"{$this->boardvert[$x][$y]}\" />";
 
                     }
                     else {                       //if this position has no image, let them choose to put an x or o there
@@ -68,41 +63,41 @@ class TicTacToe
                                     <option value=\"\"></option>
                                     <option value=\"{$this->player}\">{$this->player}</option>
                               </select>";
-
                     }
                     echo "</div>";
                 }
                 echo "<div class=\"break\"></div>";
             }
             echo "<p align=\"center\">
-                        <input type=\"submit\" name=\"move\" value=\"Take Turn\" /><br/>
+                        <br>
+                        <input type=\"submit\" name=\"move\" value=\"Take Turn\" /><br/><br/>
                         <b>It's player {$this->player}'s turn.</b></p>
                 </div>";
 
-         }
-         else {                                  //the game is over someone has won the game or there was a tie.
+        }
+        else {                                  //the game is over someone has won the game or there was a tie.
 
-                if ($this->isOver() != "Tie"){   //the game is over and someone has won the game
+            if ($this->isOver() != "Tie"){   //the game is over and someone has won the game
 
-                    echo $this->successMsg("Congratulations dude " . $this->isOver() . ", you've won the game!");
+                echo $this->successMsg("Congratulations dude " . $this->isOver() . ", you've won the game!");
 
-                }
+            }
 
-                else if ($this->isOver() == "Tie"){   //the game is over and is a tie.
+            else if ($this->isOver() == "Tie"){   //the game is over and is a tie.
 
-                    echo $this->errorMsg("Draw match play again, or solve it with a Roshambo!");
+                echo $this->errorMsg("Draw match play again, or solve it with a Roshambo!");
 
-                }
+            }
 
-                session_destroy();
+            session_destroy();
 
-                echo "<p align=\"center\"><input type=\"submit\" name=\"newgame\" value=\"New Game\" /></p>";
-         }
+            echo "<p align=\"center\"><input type=\"submit\" name=\"newgame\" value=\"New Game\" /></p>";
+        }
 
     }
 
 
-    //
+    //Process the move submitted.
     function move($request)
     {
         if ($this->isOver())
@@ -110,35 +105,35 @@ class TicTacToe
 
         $_POST = array_unique($_POST);      //remove duplicate entries on the board
 
-            foreach ($_POST as $key => $value) {            //loop through the $_POST array, looking for the submitted value.
+        foreach ($_POST as $key => $value) {            //loop through the $_POST array, looking for the submitted value.
 
-                    if ($value == $this->player) {          //checking to see if the value in that position is X or O.
+            if ($value == $this->player) {          //checking to see if the value in that position is X or O.
 
-                        $coords = explode("_", $key);       //update the board in that position with the player's X or O.
+                $coords = explode("_", $key);       //update the board in that position with the player's X or O.
 
-                        $this->boardvert[$coords[0]][$coords[1]] = $this->player;  //it is only here that the session
-                                                                                   //array is updated
+                $this->boardvert[$coords[0]][$coords[1]] = $this->player;  //it is only here that the session
+                //array is updated
 
-                        //change the turn to the next player
-                        if ($this->player == "X"){
+                //change the turn to the next player
+                if ($this->player == "X"){
 
-                            $this->player = "O";
+                    $this->player = "O";
 
-                        }
-                        else{
+                }
+                else{
 
-                            $this->player = "X";
+                    $this->player = "X";
 
-                        }
-                        $this->totalMoves++;
-                    }
+                }
+                $this->totalMoves++;
             }
+        }
 
         if ($this->isOver()){
             return;
         }
-
     }
+
 
     //Checks for ways to win the game.
     function isOver()
@@ -179,11 +174,13 @@ class TicTacToe
             return "Tie";
     }
 
+
     //Return a formatted error message
     function errorMsg($msg)
     {
         return "<div class=\"errorMsg\">$msg</div>";
     }
+
 
     //Return a formatted success message
     function successMsg($msg)
